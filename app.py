ddbd68e27c27d0f67a38a23c9d4bbe32357b7e55
tekane404 <benedictegarry@gmail.com>
2025-03-04 12:28:15 +0100

    Début scan.html

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask 
-app = Flask(__name__) 
+from flask import Flask, render_template
 
-@app.route("/") 
-def hello(): 
-    return "Bonjour bonsoir!" 
+app = Flask(__name__)
+
+@app.route("/")
+def scan():
+    return render_template("scan.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
